a0eacc30c14727e61eb756b230083f9e2958731b
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-06-11 19:32:55 +0200

    main function now parses the tokens

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from lexer import Lexer
+from parser_ import Parser
 
 while True:
     text = input("calc >")
     lexer = Lexer(text)
     tokens = lexer.generate_tokens()
-    print(list(tokens))
-
+    #print(list(tokens))
+    parser = Parser(tokens)
+    tree = parser.parse()
+    print(tree)
