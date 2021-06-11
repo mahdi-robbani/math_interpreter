c35d448832881d443bd1a7115e566573bc56cd3b
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-06-11 22:27:54 +0200

    added tree resolving to main function

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from lexer import Lexer
 from parser_ import Parser
+from interpreter import Interpreter
 
 while True:
-    text = input("calc >")
-    lexer = Lexer(text)
-    tokens = lexer.generate_tokens()
-    #print(list(tokens))
-    parser = Parser(tokens)
-    tree = parser.parse()
-    print(tree)
+    try:
+        text = input("calc >")
+        lexer = Lexer(text)
+        tokens = lexer.generate_tokens()
+        parser = Parser(tokens)
+        tree = parser.parse()
+        print(tree)
+        # If user doesn't type in anything
+        if not tree:
+            continue
+        interpreter = Interpreter()
+        value = interpreter.visit(tree)
+        print(value)
+    except Exception as e:
+        # Instead of crashing the program, just report the exception
+        print(e)
