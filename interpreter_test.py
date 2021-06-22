302ffdab39d9843ee2df9b139cb971e76a724efa
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-06-22 14:17:21 +0200

    Added tests for the interpreter

---
+++ interpreter_test.py
@@ -0,0 +1,55 @@
+import unittest
+from nodes import *
+from interpreter import Interpreter
+from values import RealNumber
+
+class TestInterpreter(unittest.TestCase):
+    """Contains all the tests for the Interpreter class"""
+
+    def test_numbers(self):
+        value = Interpreter().visit(NumberNode(12.1))
+        self.assertEqual(value, RealNumber(12.1))
+
+    def test_individual_operations(self):
+        value = Interpreter().visit(AddNode(NumberNode(3), NumberNode(7)))
+        self.assertEqual(value, RealNumber(10))
+
+        value = Interpreter().visit(SubtractNode(NumberNode(3), NumberNode(7)))
+        self.assertEqual(value, RealNumber(-4))
+
+        value = Interpreter().visit(MultiplyNode(NumberNode(3), NumberNode(7)))
+        self.assertEqual(value, RealNumber(21))
+
+        value = Interpreter().visit(DivideNode(NumberNode(3), NumberNode(7)))
+        self.assertAlmostEqual(value.value, 0.42857, 5)
+
+        value = Interpreter().visit(PowerNode(NumberNode(3), NumberNode(7)))
+        self.assertEqual(value, RealNumber(2187))
+
+        # Ensure code raises exception
+        with self.assertRaises(Exception):
+            # Divide by Zero
+            Interpreter.visit(DivideNode(NumberNode(1), NumberNode(0)))
+
+    def test_full_expression(self):
+        tree = SubtractNode(
+            AddNode(
+                NumberNode(12),
+                PowerNode(
+                    NumberNode(2),
+                    DivideNode(
+                        NumberNode(8),
+                        NumberNode(4)
+                    )
+                )
+            ),
+            MultiplyNode(
+                NumberNode(5),
+                NumberNode(3)
+            )
+        )
+        result = Interpreter().visit(tree)
+        self.assertAlmostEqual(result.value, 1.0, 5)
+
+if __name__ == '__main__':
+    unittest.main()
